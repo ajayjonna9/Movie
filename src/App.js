d85ed3d36627765976255e677ff96ad9d8387df3
AJ <jonnaajay999@gmail.com>
2022-12-09 13:21:44 +0530

    handling errors

--- src/App.js
+++ src/App.js
@@ -7,17 +7,37 @@ import axios from "axios";
 function App() {
   const [moviearr, setMoviearr] = useState([]);
   const [isLoad, setisLoad] = useState(true);
+  const [isError, setisError] = useState(false);
+  let errormsg = "Somthing went wrong";
+  let content;
   async function fetchingmovies() {
     try {
       setisLoad(false);
-      const res = await axios.get("https://swapi.py4e.com/api/films");
+      setisError(false);
+      const res = await axios.get("https://swapi.py4e.com/api/film");
 
       setMoviearr(res.data.results);
-      setisLoad(true);
+
       console.log(res.data.results);
     } catch (err) {
-      console.log("error", err);
+      setisError(true);
+
+      console.log(errormsg);
     }
+    setisLoad(true);
+  }
+  if (isLoad && moviearr.length > 0) {
+    content = <MoviesList movies={moviearr} />;
+  }
+  if (!isLoad) {
+    content = (
+      <div className="spinner">
+        <div className="loader"></div>
+      </div>
+    );
+  }
+  if (isError && isLoad) {
+    content = "Somthing went wrong";
   }
 
   return (
@@ -25,15 +45,7 @@ function App() {
       <section>
         <button onClick={fetchingmovies}>Fetch Movies</button>
       </section>
-      <section>
-        {isLoad ? (
-          <MoviesList movies={moviearr} />
-        ) : (
-          <div className="spinner">
-            <div className="loader"></div>
-          </div>
-        )}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
